38b75ac75d9668464c25d6b32254ef07aa83df5f
Zichen Wang <zichen.wang@bsu.edu>
2025-10-04 18:35:34 -0400

    1update

---
+++ project1/project1/scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export  var move_speed : float = 200
+
+func _physics_process(delta: float) -> void:
+	Input.get_vector("left","right","up","down")
+	velocity = Input.get_vector("left","right","up","down") * move_speed
+	
+	#check speed and decide run or stay
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("run")
+	
+	#control run left or right direction
+	var direction = Input.get_axis("left","right")
+	if direction:
+		velocity.x = direction * move_speed
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = direction < 0 
+	else:
+		velocity.x = move_toward(velocity.x,0,move_speed)
+	
+	
+	move_and_slide()
